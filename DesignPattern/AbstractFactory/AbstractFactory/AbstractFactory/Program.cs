4fc75af72c49889c84125d85da5a9cddc426988b
m.amiri <m.amiri@SAINST-94>
2025-05-13 16:54:25 +0330

    inst

---
+++ DesignPattern/AbstractFactory/AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,20 @@
+﻿using AbstractFactory;
+
+
+IntroduceFactoryProducts(new AppleFactory());
+
+
+IntroduceFactoryProducts(new SamsungFactory());
+
+
+void IntroduceFactoryProducts(IFactory factory)
+{
+    var smartPhone = factory.GetSmartPhone();
+
+    Console.WriteLine(smartPhone.GetDetails());
+
+    var accessory = factory.GetAccessory();
+
+    Console.WriteLine(accessory.GetDetails());
+    
+}
