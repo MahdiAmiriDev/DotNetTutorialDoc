d1984a7f882d1c1a682b45e57e7578a2f26cd3a8
unknown <mahdiamirigamefa@gmail.com>
2025-09-05 17:53:09 +0330

    home

---
+++ Grpc/Grpc-2/GrpcService/GrpcClient/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcDemo;
+using GrpcDemo.Protos;
+
+
+//var channel = GrpcChannel.ForAddress("https://localhost:7110");
+//var client = new Greeter.GreeterClient(channel);
+
+//var reply = await client.SayHelloAsync(new HelloRequest()
+//{
+//    Name = "Mahdi"
+//});
+
+//Console.WriteLine(reply.Message);
+
+
+
+//-------------------------------------------- 2 ---------------------------------
+
+//var customerGrpcChannel = GrpcChannel.ForAddress("https://localhost:7110");
+
+//var customerClient = new Customer.CustomerClient(customerGrpcChannel);
+
+//var response = await customerClient.GetCustomerInformationInStringFormatAsync(
+//    new CustomerRequest()
+//    {
+//        Name = "Parinaz",
+//        Family = "Karimi",
+//        Age = 21
+//    });
+
+//Console.ForegroundColor = ConsoleColor.Magenta;
+////Console.BackgroundColor = ConsoleColor.White;
+
+//for (int i = 0; i < 200; i++)
+//{
+//    Console.WriteLine(response.Information);
+
+//}
+
+
+//-------------------------------------------- 3 ---------------------------------
+
+
+var customerGrpcChannel = GrpcChannel.ForAddress("https://localhost:7110");
+
+var customerClient = new Customer.CustomerClient(customerGrpcChannel);
+
+using (var call = customerClient.GetCustomers(new GetCusmersRequest()))
+{
+    while (await call.ResponseStream.MoveNext())
+    {
+        var c = call.ResponseStream.Current;
+
+        Console.WriteLine($"{c.Family} {c.Name}");
+    }
+}
+
+
+
+Console.ReadKey();
---
+++ Grpc/Grpc-2/GrpcService/GrpcDemo/Services/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using GrpcDemo.Protos;
+
+namespace GrpcDemo.Services;
+
+public class CustomerService : Customer.CustomerBase
+{
+    public override Task<CustomerResponse> GetCustomerInformationInStringFormat(CustomerRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new CustomerResponse()
+        {
+            Information = "Hi " + request.Name + " " + request.Family + " your age is " +
+                              request.Age + " and I LOVE YoU"
+        });
+    }
+
+    public override async Task GetCustomers(GetCusmersRequest request, IServerStreamWriter<CusomerResponse> responseStream, ServerCallContext context)
+    {
+        List<CusomerResponse> customers = new()
+        {
+            new CusomerResponse(){Family = "amiri",Name = "mahdi"},
+            new CusomerResponse(){Family = "Jabari",Name = "ali"},
+            new CusomerResponse(){Family = "soheil",Name = "abbas"},
+            new CusomerResponse(){Family = "ghanbari",Name = "jamal"}
+        };
+
+        foreach (var customer in customers)
+        {
+            await responseStream.WriteAsync(customer);
+        }
+    }
+}
