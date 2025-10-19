66c90c0078489c8abb0ce76432ba93a3cb073362
m.amiri <m.amiri@SAINST-94>
2025-10-19 16:45:50 +0330

    inst

--- Micro Service/project/MicroServiceSample/Src/Service/IDPService/IDP.Infra/obj/Debug/net8.0/IDP.Infra.AssemblyInfo.cs
+++ Micro Service/project/MicroServiceSample/Src/Service/IDPService/IDP.Infra/obj/Debug/net8.0/IDP.Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IDP.Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5141b1c7e03684cac2778dc536144376ace6c6bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c504351ec67909f30a1db6731c8689891ddc77f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IDP.Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IDP.Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
