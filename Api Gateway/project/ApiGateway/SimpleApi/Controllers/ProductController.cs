27cb0e5d94774cd51d56cfae41f5c584d21f72d5
pc <mahdiamirigamefa@gmail.com>
2026-06-11 19:01:13 +0330

    api gateway

---
+++ Api Gateway/project/ApiGateway/SimpleApi/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("GetProduct/{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            if (id == 0)
+                return NotFound();
+
+            return Ok(new
+            {
+                Id = 1,
+                Name = "Galaxy s23 ultra",
+                CreationDate = DateTime.Now,
+                CompanyName = "Samsung",
+                Price = 12_300_00
+            });
+        }
+    }
+}
